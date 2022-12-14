b0ac813d36c4742870d346da8e30c01628e719f0
cintyy <cintiapaezdm@gmail.com>
2022-12-14 14:16:40 -0300

    agrego eventos y  funciones open / close a botones

--- js/app.js
+++ js/app.js
@@ -3,26 +3,59 @@ const $ = (selector) => document.querySelector(selector);
 //Elements 
 
 //*nav
-const burger = $("#burger");
-const modalNav = $("#modal-nav");
-const btnBalance = $("#balance");
-const btnReport = $("#report");
-const btnCategory = $("#category");
-const btnCloseModalNav = $("#close-modal-nav");
-
-
+const $btnBurger = $("#burger");
+const $modalNav = $("#modal-nav");
+const $btnBalance = $("#balance");
+const $btnReport = $("#report");
+const $btnCategory = $("#category");
+const $btnNewOp = $("#btn-new-op");
+const $btnAddNewOp = $("#btn-add-new-op");
+const $btnCancNewOp = $("#btn-canc-new-op");
+const $boxNewOp = $("#box-new-op");
+const $balance = $("#cont-balance");
 /************FUNCTIONS*****************/
-//Functions
+//Functions NAV
 
 const burgerActive = ()=>{
-    burger.classList.toggle("is-active");
-    modalNav.classList.toggle("is-active");
+    $btnBurger.classList.toggle("is-active");
+    $modalNav.classList.toggle("is-active");
+
+}
+//Functions BALANCE
+const closeBalance = () => {
+    $balance.classList.add("is-hidden");
+}
+
+const openBalance = () => {
+    $balance.classList.remove("is-hidden");
+}
+
+const boxNewOp = () => {
+   $boxNewOp.classList.remove("is-hidden");
+}
+
+const closeBoxNewOp = () => {
+    $boxNewOp.classList.add("is-hidden");
+    openBalance()
+}
+
+// const addHtml = () => {
+    
+
+// }
+const addNewOp = () => {
+    //addHtml()
+    boxNewOp()
+   closeBalance()
 
 }
 
 
 /************EVENTS*****************/
 //Events nav
-burger.addEventListener("click", burgerActive);
-console.log(burger);
+$btnBurger.addEventListener("click", burgerActive);
 
+//Events BALANCE
+$btnNewOp.addEventListener("click", addNewOp );
+$btnCancNewOp.addEventListener("click", closeBoxNewOp);
+$btnAddNewOp.addEventListener("click", closeBoxNewOp);
