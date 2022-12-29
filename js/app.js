8dd0756ea42411ad17408a3009ce85034f3dffa0
cintyy <cintiapaezdm@gmail.com>
2022-12-29 09:25:45 -0300

    corrijo datos de inputs en addLocalStorage

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const $ = (selector) => document.querySelector(selector);
 
 
 //Elements 
+//date local storage
 let dateLocalSt = JSON.parse(localStorage.getItem("operationsOB"));
 //*nav
 const $btnBurger = $("#burger");
@@ -9,6 +10,7 @@ const $modalNav = $("#modal-nav");
 const $btnBalance = $("#balance");
 const $btnReport = $("#report");
 const $btnCategory = $("#category");
+//balance
 const $btnNewOp = $("#btn-new-op");
 const $btnAddNewOp = $("#btn-add-new-op");
 const $btnCancNewOp = $("#btn-canc-new-op");
@@ -20,6 +22,7 @@ const $InewOpCategory = $("#new-op-category-filter");
 const $InewOpDate = $("#new-op-date");
 const $InewOpAmount = $("#new-op-amount");
 const $InewOpType = $("#new-op-type-filter");
+
 let operations = dateLocalSt || [];
 let operation = {
     nameOp : "",
@@ -62,75 +65,96 @@ const closeBoxNewOp = () => {
 
 const inputsDate = (e) =>{
     nameOp  = $InewOpDescrip.value 
-    amountOp= $InewOpAmount.value
+    amountOp = $InewOpAmount.value
     typeOp = $InewOpType.value
     categOp= $InewOpCategory.value
     dateOp = $InewOpDate.value
 }
 
-const colorType = () =>{
-    if(typeOp===Ganancia){
-        colorAmount = "danger" 
-    }
-    else{
-        colorAmount="primary"
-    } 
-}
+// const colorType = () =>{
+//     if(typeOp===Ganancia){
+//         colorAmount = "danger" 
+//     }
+//     else{
+//         colorAmount="primary"
+//     } 
+//     console.log(colorAmount)
+// }
 
 const addLocalStorage = () =>{
     const   inputsValues = {...operation};
-    inputsValues.nameOp;  
-    inputsValues.amountOp;     
-    inputsValues.typeOp;
-    inputsValues.categOp;
-    inputsValues.dateOp;
+    inputsValues.nameOp = nameOp;  
+    inputsValues.amountOp = amountOp;     
+    inputsValues.typeOp = typeOp;
+    inputsValues.categOp = categOp;
+    inputsValues.dateOp = dateOp;
     operations.push(inputsValues);
     localStorage.setItem("operationsOB", JSON.stringify(operations));
-
+    console.log(nameOp)
 }
-const addHtml = () => {
+/*const addHtml = () => {
 
-    $contInnerOp.innerHTML= `
+    $contInnerOp.innerHTML = `
     <div class=" is-flex">
         <div id="description" class="column">
-            <p class="subtitle">Fecha</p>
-            <p>${nameOp}</p>
-        </div>
-        <div class="container column">
-            <p class="subtitle">Categoría</p>
+            <p class="subtitle">Nombre</p>
             
         </div>
-        <div class="container column">
+        <div id="category" class="container column">
+            <p class="subtitle">Categoría</p>
+        </div>
+        <div id="date" class="container column">
             <p class="subtitle">Fecha</p>
-            <p>${dateOp}</p>
+           
         </div>
-        <div class="container column">
+        <div id="amount" class="container column">
             <p class="subtitle has-text-">Monto</p>
-            <p>${amountOp}</p>
         </div>
-        <div class="container column">
-            <p class="subtitle">Acciones</p>
-            <div class="is-flex is-right">
-                <a class="pr-4">Editar</a>
-                <a>Eliminar</a>
-            </div>
+            
+    </div>`
+        }
+//elements new
+let $$categoryOpp = $("#category");
+let $$nameOpp = $("#description");
+let $$dateOpp = $("#date");
+let $$amountOpp = $("#amount");
+
+const addOperation = () =>{
+    $contInnerOp.innerHTML += ` 
+    <div id="btn-x/a" class="container column">
+        <p class="subtitle">Acciones</p>
+        <div class="is-flex is-right">
+            <a class="pr-4">Editar</a>
+            <a>Eliminar</a>
         </div>
     </div>`
-}
+    $$categoryOpp += `<p>${nameOp}</p>`
+    $$nameOpp += `<p>${nameOp}</p>`
+    $$dateOpp += ` <p>${dateOp}</p>`
+    $$amountOpp += `<p>${amountOp}</p>`
+}*/
+// const boxBalanceV = (operations) => {
+//     const {typeOp, amountOp} = operations;
+//     if(typeOp==="new-op-factures"){
+//         total=
+//     }
+ 
+//en proceso?? 
+// }
 
 const addNewOp = () => {
-
     boxNewOp()
-   closeBalance()
+    closeBalance()
 }
 
 const addOp = () =>{
     closeBoxNewOp()
     inputsDate()
-    addHtml()
+    //addHtml()
     addLocalStorage()
+    //addOperation()
 }
-
+//no funciona inerr 
 /************EVENTS*****************/
 //Events nav
 $btnBurger.addEventListener("click", burgerActive);
